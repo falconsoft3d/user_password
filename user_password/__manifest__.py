a613e4a274832a7e1b31311d780a0ff2b2501e76
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2018-07-16 12:04:25 -0400

    Update __manifest__.py

--- user_password/__manifest__.py
+++ user_password/__manifest__.py
@@ -23,8 +23,8 @@
 {
     'name': 'Users Login MFH',
     'version': '10.0.0.1.0',
-    'author': "Falcón Solutions",
-    'maintainer': 'Falcon Solutions',
+    'author': "Falcon Solutions SpA",
+    'maintainer': 'Falcon Solutions SpA',
     'website': 'http://www.falconsolutions.cl',
     'license': 'AGPL-3',
     'category': 'Settings',
